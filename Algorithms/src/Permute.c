dea9b42423d88a456e8b63cbd020d291ecc4023a
David Lachut <dslachut@gmail.com>
2013-03-13 18:00:52 -0400

    Add permute procedure

---
+++ Algorithms/src/Permute.c
@@ -0,0 +1,35 @@
+#!/usr/bin/tcc -run
+/*
+ * Permute Array
+ *
+ *
+ *
+ */
+
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void permute(int*, int);
+
+int main(){
+  int A[4] = {9,8,7,6};
+  int n = 4;
+  int i;
+  permute(A,n);
+  for (i = 0; i < n; i++){
+    printf("%d ",A[i]);
+  }
+  printf("\n");
+}
+
+void permute(int *A, int n) {
+  int i,r,temp;
+  srand(time(NULL));
+  for (i = n-1; i > 0; i--) {
+    r = rand() % i;
+    temp = A[i];
+    A[i] = A[r];
+    A[r] = temp;
+  }
+}
